3f61a1e95c55fcaf2e618da8c017da9b2f875977
existanza <kraft.sc2@gmail.com>
2016-12-13 20:18:17 +0100

    v4

--- spot.py
+++ spot.py
@@ -1,5 +1,4 @@
 import requests
-import codecs
 from bs4 import BeautifulSoup
 import time
 import io
@@ -20,7 +19,7 @@ def from_plyrics(artist, song):
 	artist = re.sub('[^a-z]+', '', artist)
 	song = re.sub('[^a-z]+', '', song)
 	url = 'http://www.plyrics.com/lyrics/' + artist + '/' + song + '.html'
-	print(url)
+	print("Fetching the lyrics from " + url)
 	r = requests.get(url)
 	r.encoding = 'utf-8'
 	rt = r.text
@@ -37,7 +36,7 @@ def from_tekstowo(artist, song):
 	song = song.replace(" ", "_")
 	song = re.sub('[^a-z_]+', '', song)
 	url = 'http://www.tekstowo.pl/piosenka,' + artist + ',' + song + '.html'
-	print(url)
+	print("Fetching the lyrics from " + url)
 	r = requests.get(url)
 	r.encoding = 'utf-8'
 	rt = r.text
@@ -50,13 +49,17 @@ def from_tekstowo(artist, song):
 
 def get_new_lyrics(artist, song, proc):
 	from_plyrics(artist, song)
-	from_tekstowo(artist, song)
 	if not got_lyrics():
-		with io.open('lyrics.txt', 'w', encoding='utf-8') as f:
-			f.write("Got no lyrics for this song.")
+		from_tekstowo(artist, song)
+		if not got_lyrics():
+			if os.path.exists('lyrics.txt'):
+				os.remove('lyrics.txt')
+			print("Lyrics unavailable")
 	if proc is not None:
 		proc.kill()
-	proc = sp.Popen(["notepad.exe", "lyrics.txt"])
+	if os.path.exists('lyrics.txt'):
+		print("Fetched successfully")
+		proc = sp.Popen(["notepad.exe", "lyrics.txt"])
 	return artist, song, proc
 
 ac, sc = ('',)*2
@@ -71,6 +74,7 @@ while True:
 		a = a[3:]
 	s = spotilib.song().lower()
 	if a != ac or s != sc:
-		print("Getting new lyrics...")
+		print(spotilib.song_info())
 		ac, sc, pc = get_new_lyrics(a, s, pc)
+		print()
 	time.sleep(1)
